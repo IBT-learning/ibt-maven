e4d4184d8f398201b4beddbb7370e3990231a06e
Elena K <elenakazakova@yahoo.com>
2023-04-18 14:44:02 -0500

    added Elena's barnch and updated junit version

--- src/test/java/com/ibt/app/AppTest.java
+++ src/test/java/com/ibt/app/AppTest.java
@@ -7,14 +7,18 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, my name is Elena");
+
     }
 }
