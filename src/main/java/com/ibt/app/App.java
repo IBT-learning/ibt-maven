4dc57d3d4738ff1d347b9fc01ca89639ed47c302
Taiwo <twin1405jgmail.com>
2023-04-30 17:16:04 -0400

    updating App.java

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -4,8 +4,7 @@ package com.ibt.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )(System.out.println( "Hello World! this is Taiwo" ); }
-
+    public static void main( string[] args) { System.out.println( "Hello World! this is Taiwo" ); }
 }
