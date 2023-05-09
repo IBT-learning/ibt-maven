f956c96f39ff99814ed4de6e0b3125770dda3c31
Taiwo <twin1405jgmail.com>
2023-05-08 21:39:32 -0400

    update APp

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -6,5 +6,5 @@ package com.ibt.app;
  */
 public class App
 {
-    public static void main( string[] args) { System.out.println( "Hello World! this is Taiwo" ); }
+    public static void main( String[] args) { System.out.println( "Hello World! this is Taiwo" ); }
 }
