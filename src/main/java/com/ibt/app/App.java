be0987c2d4879626ca0698df9806b4c54fc8773a
Abreham <abrehamsemu@gmail.com>
2023-10-07 15:28:21 -0500

    ibt-maven

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -1,5 +1,7 @@
 package com.ibt.app;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,45 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the Simple Calculator!" );
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double first = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double second = scanner.nextDouble();
+
+        System.out.println("Select an operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                if (second != 0) {
+                    result = first / second;
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation!");
+                return;
+        }
+
+        System.out.println(first + " " + operation + " " + second + " = " + result);
     }
 }
+
