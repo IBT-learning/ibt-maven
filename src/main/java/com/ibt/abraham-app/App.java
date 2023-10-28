38a3c1a8d671ab94254781fb9ff203e39b330073
Abe <blackneymar16@gmail.com>
2023-10-28 14:51:26 -0700

    Updated code and POM

---
+++ src/main/java/com/ibt/abraham-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.abraham_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is abraham" );
+    }
+}
