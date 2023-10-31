582347a08864c1ba527497d58187215f3e053956
Morel Sami <mksami237@gmail.com>
2023-10-31 18:00:43 -0400

    Greetings to my fellow comrades

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World this is Chinwe! ");
+        System.out.println( "Hello World this is Morel Sami! ");
+        System.out.println("I am very excited about this DevOps Training program!");
     }
 }
