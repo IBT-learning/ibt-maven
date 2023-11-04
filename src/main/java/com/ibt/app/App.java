323f6a9a64b4e683b87789f1e0f9dd6d1ba6eaa2
chisom <somokehie28@gmail.com>
2023-11-04 16:23:48 -0400

    updating pom+app.java

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -4,10 +4,10 @@ package com.ibt.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World this is Chinwe! ");
+        System.out.println( "Hello World this is Chisom O! ");
     }
 }
