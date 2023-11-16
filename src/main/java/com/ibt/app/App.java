710328b17bbdfbda7d53586e40459bacff7a2c9e
Daniel Brown <dannybrow317@gmail.com>
2023-11-16 00:41:08 -0500

    updating pom+app.java

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -4,10 +4,8 @@ package com.ibt.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World this is Chinwe! ");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World this is dannybrown317!");
     }
 }
